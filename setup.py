85e213476b20b63f4bb85e339f48a3ae87e007a5
GalaxyXieyu <523018705@qq.com>
2025-02-24 20:32:37 +0800

    chore: add test.ipynb to gitignore and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="didatodolist",
-    version="0.1.41",
+    version="0.1.42",
     author="xieyu",
     author_email="523018705@qq.com",
     description="滴答清单(TickTick/Dida365) Python SDK",
