b092c0bb5d1395065365d2a66bc9e0bbad7519f9
danielxieyu <523018705@qq.com>
2025-03-03 23:05:07 +0800

    Release version 0.2.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="didatodolist",
-    version="0.2.7",
+    version="0.2.8",
     author="xieyu",
     author_email="523018705@qq.com",
     description="滴答清单(TickTick/Dida365) Python SDK",
